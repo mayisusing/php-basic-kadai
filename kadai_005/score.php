dab31b966b4c3a1addfdadfef9d5e972381077de
mayisusing <kellylakis1215@gmail.com>
2024-10-02 21:02:31 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <p>
+    <?php
+    $score_total = 80 + 60 + 55 + 40 + 100 + 25 + 80 + 95 + 30 + 60;
+    $score_average = $score_total / 10
+    ?>
+    </p>
+</body>
+</html>
